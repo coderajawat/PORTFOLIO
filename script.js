396b750655046919dbd4542baf2f318e2e6ffd07
coderajawat <khushirajawat755@gmail.com>
2024-03-13 12:35:26 +0530

    PORTFOLIO

---
+++ script.js
@@ -0,0 +1,12 @@
+var typeData = new Typed(".role", {
+    strings: [
+        "Front End Developer",
+        "Web Developer",
+        "UI-UX Designer",
+        "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+});
